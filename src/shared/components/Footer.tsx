95f9084cafe7f1397dae045d920c7687b2010c96
Joao-Eduardo-AJ <joaoeduaj@gmail.com>
2023-04-12 16:22:42 -0300

    feat: adds footer component

---
+++ src/shared/components/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from '../../styles/Footer.module.css';
+
+export const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>
+        <span>PokeNext</span> &copy; 2023
+      </p>
+    </footer>
+  );
+};
