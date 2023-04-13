01faab3828720be3827561d19b598cce3e51ca04
Joao-Eduardo-AJ <joaoeduaj@gmail.com>
2023-04-12 21:49:09 -0300

    feat: update homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,23 @@
-export default function Home() {
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+
+const Home = () => {
   return (
-    <>
-      <h1>AAA</h1>
-    </>
+    <article>
+      <div className={styles.titleContainer}>
+        <p>Poke</p>
+        <p>Next</p>
+        <Image
+          className={styles.image}
+          src="/images/pokeball.png"
+          width={50}
+          height={50}
+          alt="PokeNext"
+        />
+      </div>
+      <p>Cards</p>
+    </article>
   );
-}
+};
+
+export default Home;
