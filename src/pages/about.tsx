d3f890ad12be66cd689be0a1fe4f40aaa09e66ad
Joao-Eduardo-AJ <joaoeduaj@gmail.com>
2023-04-12 21:47:49 -0300

    feat: adds about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,19 @@
+import Image from 'next/image';
+import styles from '../styles/About.module.css';
+
 const About = () => {
-  return <h1>About</h1>;
+  return (
+    <article className={styles.about}>
+      <h1>About the project</h1>
+      <p>PokeNext is an App made with Next.js to search for Pokémons.</p>
+      <Image
+        src="/images/charizard.png"
+        alt="Charizard"
+        width={440}
+        height={410}
+      />
+    </article>
+  );
 };
 
 export default About;
