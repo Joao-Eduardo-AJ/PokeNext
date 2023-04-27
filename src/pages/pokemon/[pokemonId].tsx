0292274f08a214eed671661ee11503b81661555b
Joao-Eduardo-AJ <joaoeduaj@gmail.com>
2023-04-27 20:19:29 -0300

    feat: adds pokemon dynamic page

---
+++ src/pages/pokemon/[pokemonId].tsx
@@ -0,0 +1,100 @@
+import styles from '../../styles/Pokemon.module.css';
+import Image from 'next/image';
+import { GetStaticPaths, GetStaticProps } from 'next';
+
+interface result {
+  results: [
+    {
+      id: number;
+      url: string;
+    }
+  ];
+}
+
+type PokemonType = {
+  name: string;
+  id: number;
+  types: {
+    type: {
+      name: string;
+    };
+  }[];
+  height: number;
+  weight: number;
+};
+
+type PokemonProps = {
+  pokemon: PokemonType;
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const maxPokemons = 151;
+  const api = 'https://pokeapi.co/api/v2/pokemon/';
+
+  const result = await fetch(`${api}/?limit=${maxPokemons}`);
+  const data: result = await result.json();
+
+  const paths = data.results.map((pokemon, index) => {
+    return {
+      params: { pokemonId: (index + 1).toString() },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async context => {
+  const id = context.params?.pokemonId;
+
+  const result = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+
+  const data = await result.json();
+
+  return {
+    props: { pokemon: data },
+  };
+};
+
+const Pokemon = ({ pokemon }: PokemonProps) => {
+  return (
+    <div className={styles.pokemonPageContent}>
+      <article>
+        <h1>{pokemon.name}</h1>
+        <Image
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+          width={200}
+          height={200}
+          alt={pokemon.name}
+        />
+        <h3>Número</h3>
+        <p>{`#${pokemon.id}`}</p>
+        <h3>Tipo</h3>
+        <div>
+          {pokemon.types.map((item, index) => (
+            <span
+              key={index}
+              className={`${styles.type} ${styles['type_' + item.type.name]}`}
+            >
+              {item.type.name}
+            </span>
+          ))}
+        </div>
+      </article>
+      <section>
+        <div>
+          <h4>Altura</h4>
+          <p>{pokemon.height * 10}cm</p>
+        </div>
+        <div>
+          <h4>Peso</h4>
+          <p>{pokemon.weight / 10}Kg</p>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Pokemon;
