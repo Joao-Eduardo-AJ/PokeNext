da2f1c0431ea8f76546865307ee0d9c6f0a10b4d
Joao-Eduardo-AJ <joaoeduaj@gmail.com>
2023-05-23 22:59:07 -0300

    feat: update navbar component

--- src/shared/components/Navbar.tsx
+++ src/shared/components/Navbar.tsx
@@ -3,30 +3,23 @@ import Link from 'next/link';
 
 import styles from '../../styles/Navbar.module.css';
 
-export const Navbar = () => {
-  return (
-    <nav className={styles.navBar}>
-      <div className={styles.logo}>
-        <Image
-          src="/images/pokeball.png"
-          width="30"
-          height="30"
-          alt="PokeNext"
-        />
-        <h1>PokeNext</h1>
-      </div>
-      <ul className={styles.linkItems}>
-        <li>
-          <Link href="/" className={styles.link}>
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link href="/about" className={styles.link}>
-            About
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  );
-};
+export const Navbar = () => (
+  <nav className={styles.navBar}>
+    <div className={styles.logo}>
+      <Image src="/images/pokeball.png" width="30" height="30" alt="PokeNext" />
+      <h1>PokeNext</h1>
+    </div>
+    <ul className={styles.linkItems}>
+      <li>
+        <Link href="/" className={styles.link}>
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link href="/about" className={styles.link}>
+          About
+        </Link>
+      </li>
+    </ul>
+  </nav>
+);
