8e5abb0b9d404347be3b48476c3c37cfd9b0e198
Joao-Eduardo-AJ <joaoeduaj@gmail.com>
2023-05-26 12:55:13 -0300

    feat: adds react toastify lib to show error alert

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,17 @@
 import 'swiper/css';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import 'swiper/css/effect-coverflow';
 import { useRouter } from 'next/router';
 import { EffectCoverflow } from 'swiper';
-import { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import styles from '../styles/Home.module.css';
 import { SearchInput, Card } from '@/shared/components';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { TextsProvider } from '@/translation';
 interface result {
   results: [
@@ -57,9 +59,15 @@ const Home = ({ pokemons }: PokemonProps) => {
   const [pageWidth, setPageWidth] = useState(0);
   const router = useRouter();
   const texts = TextsProvider.get();
+  const notify = () => toast.error(texts.INVALID_NAME);
 
   const handleSearch = (search: string) => {
-    if (!isNaN(Number(search)) && search !== '') {
+    if (
+      !isNaN(Number(search)) &&
+      search !== '' &&
+      Number(search) < 1011 &&
+      Number(search) > 0
+    ) {
       router.push(`/pokemon/${search}`);
     } else {
       const pokemonId = pokemons.find(
@@ -68,7 +76,7 @@ const Home = ({ pokemons }: PokemonProps) => {
       if (pokemonId) {
         router.push(`/pokemon/${pokemonId}`);
       } else {
-        alert(texts.INVALID_NAME);
+        notify();
       }
     }
   };
@@ -92,6 +100,16 @@ const Home = ({ pokemons }: PokemonProps) => {
   }
   return (
     <div className={styles.homeContent}>
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar
+        closeOnClick
+        closeButton={false}
+        draggable
+        pauseOnFocusLoss
+        theme="dark"
+      />
       <article>
         <div className={styles.titleContainer}>
           <p>Poke</p>
